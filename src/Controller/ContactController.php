b7c96ddee7e02867a5786e4eb6b95c16cf1408e6
TOUGLO <donhonoros10@gmail.com>
2019-05-21 23:02:04 +0200

    Api de get et de post

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use App\Entity\Contact;
+
+/**
+ * Contact controller.
+ * @Route("/api",name="api_")
+ */
+class ContactController extends FOSRestController
+{
+    /**
+     * Lists all Contacts.
+     * @Rest\Get("/contacts")
+     *
+     * @return Response
+     */
+    public function getContactAction()
+    {
+        $repository = $this->getDoctrine()->getRepository(Contact::class);
+            $contacts = $repository->findAll();
+        return $this->handleView($this->view($contacts));
+    }
+
+    /**
+     * Create Contact.
+     * @Rest\Post("/contact")
+     *
+     * @return Response
+     */
+    public function postContactAction(Request $request, \Swift_Mailer $mailer)
+    {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+
+        $data = json_decode($request->getContent(), true);
+        $form->submit($data);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $departement  = $contact->getIdDepartement();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($contact);
+            $em->flush();
+
+            $message = (new \Swift_Message('Enregistrement du nouveau contact'))
+                ->setFrom('donhonoros10@gmail.com')
+                ->setTo($departement->getEmailDepartement())
+                ->setBody($this->renderView('emails/contact.html.twig', [
+                    'nom' => $contact->getNom(),
+                    'prenom' => $contact->getPrenom(),
+                    'message' => $contact->getMessage(),
+                    'email' => $contact->getEmail()
+                ]),'text/html');
+            $mailer->send($message);
+            return $this->handleView($this->view(['status' => 'ok'], Response::HTTP_CREATED));
+        }
+
+        return $this->handleView($this->view($form->getErrors()));
+    }
+
+
+
+}
---
+++ src/Entity/Contact.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="contact")
+ */
+class Contact{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
+     */
+    private $nom;
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
+     */
+    private $prenom;
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
+     */
+    private $email;
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
+     */
+    private $message;
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Departement", inversedBy="contacts")
+     */
+    private $id_departement;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     * @return Contact
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     * @return Contact
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * @param mixed $prenom
+     * @return Contact
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     * @return Contact
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param mixed $message
+     * @return Contact
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdDepartement()
+    {
+        return $this->id_departement;
+    }
+
+    /**
+     * @param mixed $id_departement
+     * @return Contact
+     */
+    public function setIdDepartement($id_departement)
+    {
+        $this->id_departement = $id_departement;
+        return $this;
+    }
+
+}
